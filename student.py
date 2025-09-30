0728fc011beac0107e0e069a9765935092706e83
riyalagare8-rgb <riyalagare8@gmail.com>
2025-09-30 09:42:08 +0530

    save to main branch

---
+++ student.py
@@ -0,0 +1,14 @@
+# Define student details using a dictionary
+student = {
+    "Name": "riya lagare",
+    "Age": 20,
+    "Roll Number": "022",
+    "Branch": "bca",
+    "College": "kle tech"
+}
+
+# Print student details
+print("📘 Student Details:")
+print("-------------------")
+for key, value in student.items():
+    print(f"{key}: {value}")
